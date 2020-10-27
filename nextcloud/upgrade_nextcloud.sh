52d48a5ffebb9bc0c26652d37d51299ad4fe7d11
root <root@cloud-storage-01>
2020-10-27 11:48:23 +0000

    update upgrade scrupt

--- nextcloud/upgrade_nextcloud.sh
+++ nextcloud/upgrade_nextcloud.sh
@@ -12,7 +12,7 @@ nextcloud_sysuser=$(read_param nextcloud sysuser)
 nextcloud_current_dir=$(read_param nextcloud parent_pathdir)
 nextcloud_app_dir=$(read_param nextcloud appdir_name)
 nextcloud_wanted_version=$(read_param nextcloud wanted_version)
-nextcloud_current_version=$(sudo -u $nextcloud_sysuser php $nextcloud_current_dir/$nextcloud_app_dir/occ --version | tail -n1 | cut -d ' ' -f2)
+nextcloud_current_version=$(sudo -u $nextcloud_sysuser php $nextcloud_current_dir/$nextcloud_app_dir/occ --version | tail -n1 | cut -d ' ' -f3)
 nextcloud_backup_dir=/opt/nextcloud-$nextcloud_current_version-backup-`date +"%Y%m%d"`
 
 nextcloud_dbuser=$(read_param nextcloud dbuser)
@@ -37,9 +37,9 @@ mysqldump --single-transaction -h $nextcloud_dbhost -u $nextcloud_dbuser -p$next
 systemctl stop nginx
 mv $nextcloud_current_dir/$nextcloud_app_dir  $nextcloud_current_dir/$nextcloud_app_dir-$nextcloud_current_version-old
 wget -O- https://download.nextcloud.com/server/releases/nextcloud-$nextcloud_wanted_version.zip > $nextcloud_current_dir/nextcloud-$nextcloud_wanted_version.zip
-unzip $nextcloud_current_dir/nextcloud-$nextcloud_wanted_version.zip
+unzip $nextcloud_current_dir/nextcloud-$nextcloud_wanted_version.zip -d $nextcloud_current_dir/.
 cp $nextcloud_current_dir/$nextcloud_app_dir-$nextcloud_current_version-old/config/config.php $nextcloud_current_dir/$nextcloud_app_dir/config/config.php
-chmod -R $nextcloud_sysuser:www-data $nextcloud_current_dir/$nextcloud_app_dir
+chown -R $nextcloud_sysuser:www-data $nextcloud_current_dir/$nextcloud_app_dir
 systemctl start nginx
 sudo -u $nextcloud_sysuser php $nextcloud_current_dir/$nextcloud_app_dir/occ upgrade
 
